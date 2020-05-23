871a02e9215fd74c8146eaa2efcdd30155a25428
Rafael Oliveira <rafaelbatistacg@gmail.com>
2020-05-23 14:57:36 -0300

    saving a new post

--- src/main/java/com/spring/codeblog/controllers/CodeBlogController.java
+++ src/main/java/com/spring/codeblog/controllers/CodeBlogController.java
@@ -4,11 +4,16 @@ import com.spring.codeblog.models.Post;
 import com.spring.codeblog.service.CodeBlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -31,6 +36,22 @@ public class CodeBlogController {
         Post post = codeBlogService.findById(id);
         mv.addObject("post", post);
         return mv;
+    }
+
+    @RequestMapping(value="/newpost", method=RequestMethod.GET)
+    public String getPostForm(){
+        return "postForm";
+    }
+
+    @RequestMapping(value="/newpost", method=RequestMethod.POST)
+    public String savePost(@Validated Post post, BindingResult result, RedirectAttributes attributes){
+        if(result.hasErrors()){
+            attributes.addFlashAttribute("mensagem", "Verifique se os campos obrigatórios foram preenchidos!");
+            return "redirect:/newpost";
+        }
+        post.setDate(LocalDate.now());
+        codeBlogService.save(post);
+        return "redirect:/posts";
     }
 
 }
